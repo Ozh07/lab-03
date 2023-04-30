a61fe7de478179116c93a965357dbfc19ce3a66d
Ozh07 <olzh4711@gmail.com>
2023-04-30 22:22:51 +0300

    lab3

--- src/main/java/edu/kau/fcit/cpit252/App.java
+++ src/main/java/edu/kau/fcit/cpit252/App.java
@@ -1,6 +1,7 @@
 package edu.kau.fcit.cpit252;
 
 public class App {
+    
     public static void main(String[] args) {
         Car c1 = new Car("Honda", "Accord", 2019);
         // print car and recall info
